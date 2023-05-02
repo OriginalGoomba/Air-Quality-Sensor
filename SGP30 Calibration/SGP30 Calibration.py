d20ea4a97dda2d558421163808a55a75551ee56c
Dillon <59833207+OriginalGoomba@users.noreply.github.com>
2023-05-01 22:14:07 -0400

    Update SGP30 Calibration.py

--- SGP30 Calibration/SGP30 Calibration.py
+++ SGP30 Calibration/SGP30 Calibration.py
@@ -22,6 +22,8 @@ i2c = board.STEMMA_I2C()
 # Create library object on our I2C port
 sgp30 = adafruit_sgp30.Adafruit_SGP30(i2c)
 
+time.sleep(3)
+
 print("SGP30 serial #", [hex(i) for i in sgp30.serial])
 
 
@@ -32,32 +34,46 @@ sgp30.set_iaq_relative_humidity(celsius=22.1, relative_humidity=48)
 elapsed_sec = 0
 
 try:
-    with open("/baselines.txt", "a") as fp:
-
-        # read baselines for SGP30
-        #for line in fp:
-        #    pass
-        #last_line = line
-        #print(last_line)
-
+    with open("/baselines.txt", "r") as fp:
+        
+        baselines = fp.readlines()
+        baseline_eCO2 = int(baselines[0])
+        baseline_TVOC = int(baselines[1])
+        print("Reading from baselines.txt")
+        print(baseline_eCO2)
+        print(type(baseline_eCO2))
+        print(baseline_TVOC)
+        print(type(baseline_TVOC))
+        print("--------------------------")
+        print("Baseline type for eCO2:")
+        print(type(sgp30.baseline_eCO2))
+        fp.close()
+        
+        if baseline_eCO2 != 0 and baseline_TVOC != 0:
+            print("Setting baselines from file...")
+            sgp30.set_iaq_baseline(baseline_eCO2, baseline_TVOC)
+            print(sgp30.get_iaq_baseline())
+        
         while True:
-
+            
             print("eCO2 = %d ppm \t TVOC = %d ppb" % (sgp30.eCO2, sgp30.TVOC))
             time.sleep(1)
             elapsed_sec += 1
             if elapsed_sec > 10:
+                fp = open("/baselines.txt", "w")
                 elapsed_sec = 0
                 print(
-                "**** Baseline values: eCO2 = 0x%x, TVOC = 0x%x"
+                "New baseline values: eCO2 = 0x%x, TVOC = 0x%x"
                 % (sgp30.baseline_eCO2, sgp30.baseline_TVOC)
                 )
                 print(sgp30.get_iaq_baseline())
 
                 #temp = microcontroller.cpu.temperature
                 # do the C-to-F conversion here if you would like
-                fp.write('0x{0:x}, 0x{0:x}'.format(sgp30.baseline_eCO2, sgp30.baseline_TVOC))
+                fp.write('{:d}\n'.format(sgp30.baseline_eCO2))
+                fp.write('{:d}\n'.format(sgp30.baseline_TVOC))
                 # fp.write(sgp30.baseline_eCO2 + ", " + sgp30.baseline_TVOC + "\n")
-                fp.flush()
+                fp.close()
                 led.value = not led.value
                 time.sleep(1)
 except OSError as e:  # Typically when the filesystem isn't writeable...
